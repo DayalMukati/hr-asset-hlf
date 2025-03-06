b4b317cc6e918f35ddc622284a5a2bc1e05613e8
Dayal Mukati <dayalmukati@gmail.com>
2025-03-06 22:26:17 +0530

    chaincode update

--- chaincode/asset.go
+++ chaincode/asset.go
@@ -22,22 +22,127 @@ type Token struct {
 
 // MintTokens creates new tokens for an asset
 func (s *SmartContract) MintTokens(ctx contractapi.TransactionContextInterface, assetID string, amountStr string, owner string) error {
-	
+	amount, err := strconv.Atoi(amountStr)
+	if err != nil {
+		return fmt.Errorf("invalid amount")
+	}
+
+	token := Token{
+		AssetID: assetID,
+		Owner:   owner,
+		Amount:  amount,
+	}
+
+	tokenJSON, err := json.Marshal(token)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(assetID, tokenJSON)
 }
 
 // TransferTokens transfers tokens from one owner to another
 func (s *SmartContract) TransferTokens(ctx contractapi.TransactionContextInterface, assetID string, transferAmountStr string, newOwner string) error {
-	
+	transferAmount, err := strconv.Atoi(transferAmountStr)
+	if err != nil {
+		return fmt.Errorf("invalid transfer amount")
+	}
+
+	tokenJSON, err := ctx.GetStub().GetState(assetID)
+	if err != nil {
+		return fmt.Errorf("failed to read asset: %v", err)
+	}
+	if tokenJSON == nil {
+		return fmt.Errorf("asset does not exist")
+	}
+
+	var token Token
+	err = json.Unmarshal(tokenJSON, &token)
+	if err != nil {
+		return err
+	}
+
+	if token.Amount < transferAmount {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	token.Amount -= transferAmount
+	newToken := Token{
+		AssetID: assetID,
+		Owner:   newOwner,
+		Amount:  transferAmount,
+	}
+
+	updatedTokenJSON, err := json.Marshal(token)
+	if err != nil {
+		return err
+	}
+	newTokenJSON, err := json.Marshal(newToken)
+	if err != nil {
+		return err
+	}
+
+	ctx.GetStub().PutState(assetID, updatedTokenJSON)
+	ctx.GetStub().PutState(assetID+"_"+newOwner, newTokenJSON)
+
+	return nil
 }
 
 // BurnTokens removes tokens from circulation
 func (s *SmartContract) BurnTokens(ctx contractapi.TransactionContextInterface, assetID string, burnAmountStr string) error {
-	
+	burnAmount, err := strconv.Atoi(burnAmountStr)
+	if err != nil {
+		return fmt.Errorf("invalid burn amount")
+	}
+
+	tokenJSON, err := ctx.GetStub().GetState(assetID)
+	if err != nil {
+		return fmt.Errorf("failed to read asset: %v", err)
+	}
+	if tokenJSON == nil {
+		return fmt.Errorf("asset does not exist")
+	}
+
+	var token Token
+	err = json.Unmarshal(tokenJSON, &token)
+	if err != nil {
+		return err
+	}
+
+	if token.Amount < burnAmount {
+		return fmt.Errorf("insufficient balance to burn")
+	}
+
+	token.Amount -= burnAmount
+	if token.Amount == 0 {
+		return ctx.GetStub().DelState(assetID)
+	}
+
+	updatedTokenJSON, err := json.Marshal(token)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(assetID, updatedTokenJSON)
 }
 
 // GetAssetBalance retrieves the balance of an asset
 func (s *SmartContract) GetAssetBalance(ctx contractapi.TransactionContextInterface, assetID string) (*Token, error) {
-	
+	tokenJSON, err := ctx.GetStub().GetState(assetID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read asset: %v", err)
+	}
+	if tokenJSON == nil {
+		return nil, fmt.Errorf("asset does not exist")
+	}
+
+	var token Token
+	err = json.Unmarshal(tokenJSON, &token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
 }
 
 func main() {
